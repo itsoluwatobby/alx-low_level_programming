9a76c31d37a4d5a4b07c632a97b0e10e7fb5b83f
oluwatobi <itsoluwatobby@gmail.com>
2023-03-17 09:16:20 -0700

    done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,12 +10,19 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		for (j = i; j < 100; j++)
+		int e = i + j;
+
+		for (j = 0; j < 100; j++)
 		{
 			int a = i / 10;
 			int b = i % 10;
 			int c = j / 10;
 			int d = j % 10;
+
+			if (e < (i + j) || (a + b) == (c + d))
+				continue;
+			else if ((a + b) > (c + d))
+				continue;
 
 			putchar('0' + a);
 			putchar('0' + b);
