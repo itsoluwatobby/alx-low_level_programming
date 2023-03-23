0d8297acb0a3155ecbde816b07bc0b307237c862
oluwatobi <itsoluwatobby@gmail.com>
2023-03-23 13:40:23 +0100

    update

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,21 +8,21 @@ void print_triangle(int size)
 	int i;
 	int k;
 
-	if (size <= 0)
-		_putchar('\n');
-
-	for (i = 1; i <= size; i++)
+	if (size > 0)
 	{
-		for (k = size - i; k > 0; k--)
-			_putchar(' ');
+		for (i = 1; i <= size; i++)
+		{
+			for (k = size - i; k > 0; k--)
+				_putchar(' ');
 
-		for (k = 0; k < i; k++)
-			_putchar('#');
+			for (k = 0; k < i; k++)
+				_putchar('#');
 
-		if (i == size)
-			continue;
+			if (i == size)
+				continue;
 
-		_putchar('\n');
+			_putchar('\n');
+		}
 	}
 	_putchar('\n');
 }
