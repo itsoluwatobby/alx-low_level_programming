b0064ae185dc92f21087378dc3a48774e9e1aa6f
oluwatobi <itsoluwatobby@gmail.com>
2023-03-27 16:30:11 +0100

    string reversal

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,12 +9,19 @@
 
 void puts_half(char *str)
 {
-	long int len =  strlen(str);
+	int len = strlen(str);
 	int i;
 
 	if ((len % 2) == 0)
 	{
 		for (i = len / 2; i < len; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		int div = round(len / 2);
+
+		for (i = div; i < len; i++)
 			_putchar(str[i]);
 	}
 
