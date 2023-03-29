f486d0dad9a939ac54d8606e975c68d8219acb91
oluwatobi <itsoluwatobby@gmail.com>
2023-03-29 08:50:47 +0100

    continuation

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * leet - function that encodes a string into 1337.
+ * @str: first parameter
+ * Return: encoded string
+ */
+
+char *leet(char *str)
+{
+	int i;
+
+	while (str)
+	{
+	return (str);
+}
