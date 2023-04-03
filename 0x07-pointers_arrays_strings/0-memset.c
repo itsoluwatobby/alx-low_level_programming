876f214d0855a39644aea35c9c58c56d18ea3b4e
oluwatobi <itsoluwatobby@gmail.com>
2023-04-03 09:33:41 +0100

    task 7 initialisation

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _memset - function that fills memory with a constant byte.
+ * @s: first parameter
+ * @b: second parameter
+ * @n: third parameter
+ * Return: pointer to s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (s);
+}
