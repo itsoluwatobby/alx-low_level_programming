5a552f0563fc8cade465078cdb9b0cdcede07d4b
oluwatobi <itsoluwatobby@gmail.com>
2023-04-11 09:58:44 -0700

    malloc done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,7 +22,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i] || s1[i]; i++)
 		len++;
 
-	concat = malloc(sizeof(char) * len);
+	concat = malloc(sizeof(char) * len - 3);
 
 	if (concat == NULL)
 		return (NULL);
