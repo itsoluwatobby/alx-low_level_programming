003e9ec2c69df07745f229f61f540258513b09a3
itsoluwatobby <itsoluwatobby@gmail.com>
2023-04-13 18:10:48 +0100

    dc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,18 +13,12 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *new_value;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0, index;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-
-	else if (s2 == NULL)
-		s2 = "";
-
-	for (index = 0; s1[index]; index++)
+	while (s1 && s1[len1])
 		len1++;
 
-	for (index = 0; s2[index]; index++)
+	while (s2 && s2[len2])
 		len2++;
 
 	if (n < len2)
