0df5409834b51504bdb4362b77e2664fa51199d6
itsoluwatobby <itsoluwatobby@gmail.com>
2023-04-17 15:36:16 +0100

    done

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,1 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/*
+ * File: 3-function_like_macro.h
+ * Desc: Header file that defines a function-like-macro named
+ *       ABS, that conputes the absolute value of number.
+ */
+
 #define ABS(x) ((x) < (0) ? (x) * (-1) : (x))
+
+#endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,1 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Desc: Header file that defines a macro-like function named
+ *       Sum, to compute the addition to two variables.
+ */
+
 #define SUM(x, y) ((x) + (y))
+
+#endif
