78ecba7c4a3bf69e241a09bf2a3f54e2ceaa19a3
itsoluwatobby <itsoluwatobby@gmail.com>
2023-04-17 15:21:13 +0100

    macro

---
+++ 0x0D-preprocessor/create.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo -e "Enter file name(s): \c"
+read -a file_names
+
+if [ "${#file_names[@]}" -gt 0 ]
+then
+	touch "${file_names[@]}"
+        chmod a+x "${file_names[@]}"
+        echo "Files [${file_names[@]}] created successfully and execute permission granted"
+else	
+	echo "File name(s) required"
+fi
