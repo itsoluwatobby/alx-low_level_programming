96a7acfa8b42db295dabb2aef20bd0ccde44ac19
itsoluwatobby <itsoluwatobby@gmail.com>
2023-04-27 14:49:55 +0100

    completed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,26 +1,26 @@
 #include "lists.h"
 #include <stdio.h>
-#include <stddef.h>
 
 /**
  * print_list - function that prints all the elements of a list_t list.
  * @h: given parameter
+ *
  * Return: number of nodes in h
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t node = 0;
 
 	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
-		nodes++;
+		node++;
 		h = h->next;
 	}
 
-	return (nodes);
+	return (node);
 }
--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,13 @@
-#include "lists.h"
 #include <stdio.h>
 
-void print_before_main(void) __attribute__((constructor))
+void before_main(void) __attribute__((constructor));
 
 /**
- * print_before_main - a function that runs before the main funtion
+ * print_before_main - a function that runs before the main funtion.
  */
 
-void print_before_main(void)
+void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
