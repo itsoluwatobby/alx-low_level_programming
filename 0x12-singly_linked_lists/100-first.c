b61c7464eb64b5f204f297243a6806a00bd55c7a
itsoluwatobby <itsoluwatobby@gmail.com>
2023-04-27 14:53:59 +0100

    done

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,11 +3,11 @@
 void before_main(void) __attribute__((constructor));
 
 /**
- * print_before_main - a function that runs before the main funtion.
+ * before_main - a function that runs before the main funtion.
  */
 
 void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
